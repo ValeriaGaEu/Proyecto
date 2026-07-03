a08e1c76580b9560cc8c645704c5f63bd94511b8
ValeriaGaEu <25290823@utcalvillo.edu.mx>
2026-07-03 11:47:06 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/hydroponicgarden1/HydroponicGarden1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.hydroponicgarden1;
+
+/**
+ *
+ * @author valer
+ */
+public class HydroponicGarden1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
